4e798dcf41f41bf9c95b9de1111a6f436b6a62a3
Vrushali_Sonawane <mivrushalis@gmail.com>
2026-01-16 11:55:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (40.34%) - LeetSync

--- 344-reverse-string/reverse-string.c
+++ 344-reverse-string/reverse-string.c
@@ -1,16 +1,15 @@
 void reverseString(char* s, int sSize) 
 {
-       int start = 0;
-       int end = sSize - 1;
-       
-       while(start <= end)
-       {
-          char temp = s[start];
-          s[start] = s[end];
-          s[end] = temp;
+   int start = 0;
+   int end = sSize - 1;
+   char temp;
+   while(start <= end)
+   {
+     temp = s[end];
+     s[end] = s[start];
+     s[start] = temp;
 
-          start++;
-          end--;
-       }
-
+     start++;
+     end--;
+   }    
 }
